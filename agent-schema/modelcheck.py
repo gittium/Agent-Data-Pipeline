a09fdaad49324e6b34de205a9e66354c677eef6d
gittium <umcode123a@gmail.com>
2025-10-15 18:22:18 +0700

    newmodelcheck

--- agent-schema/modelcheck.py
+++ agent-schema/modelcheck.py
@@ -1,5 +1,5 @@
 from google import generativeai
-generativeai.configure(api_key="AIzaSyCyxeKeSKcTbXop9y2wD7jx5-1tIqaJMho")  
+generativeai.configure(api_key="")  
 models = generativeai.list_models()  
 for m in models:
     if "generateContent" in m.supported_generation_methods:
